c075e12c8ef36eb46195f8279fe2349861f44090
Piyush Gupta <pgtweety1234@gmail.com>
2023-06-25 11:30:13 +0530

    login added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { user,loginWithRedirect,logout,isAuthenticated } = useAuth0();
+  return <>
+  <div className="list">
+      {
+        isAuthenticated && 
+        <li><p className="para">Welcome , {user.name}</p></li>
+      }
+    
+    {
+      isAuthenticated ? (<li>
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+          Log Out
+        </button>
+        </li>) : (<li>
+    <button onClick={() => loginWithRedirect()}>Log In</button>
+    </li>)
+    }
+    
+    
+  </div>
+  </>;
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  
+   <Auth0Provider
+    domain="dev-x3al2aqvpgpv36ta.us.auth0.com"
+    clientId="UispeXNY0f5XXwnYTpTxlodQT3BKNFrf"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
+  
 );

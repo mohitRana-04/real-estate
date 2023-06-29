3cbe6d51ca321ad7cc40a22438e51c62d2c6e2a1
Piyush Gupta <pgtweety1234@gmail.com>
2023-06-29 12:21:05 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Navbar } from './components/Navbar';
+import  Navbar  from './components/Navbar';
 import { Router, Route, Switch } from "react-router-dom";
 
 import { Footer } from './components/Footer';
